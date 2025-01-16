d94b8ac57fcbe1b8c8cbbc008715489947ea64be
Franco Zabala <asfalso@gmail.com>
2025-01-16 15:57:16 -0300

    add some responsiveness

--- src/components/AddressInput/AddressInput.tsx
+++ src/components/AddressInput/AddressInput.tsx
@@ -8,10 +8,16 @@ type AddressInputProps = {
 export const AddressInput = ({ disabled }: AddressInputProps) => {
   return (
     <div className={styles["address_input"]}>
-      <Input name="address.street" disabled={disabled} label="Street" />
-      <Input name="address.city" disabled={disabled} label="City" />
-      <Input name="address.state" disabled={disabled} label="State" />
-      <Input name="address.zip" disabled={disabled} label="Zip" />
+      <div className="wrapper">
+        <div className="wrapper-column">
+          <Input name="address.street" disabled={disabled} label="Street" />
+          <Input name="address.city" disabled={disabled} label="City" />
+        </div>
+        <div className="wrapper-column">
+          <Input name="address.state" disabled={disabled} label="State" />
+          <Input name="address.zip" disabled={disabled} label="Zip" />
+        </div>
+      </div>
     </div>
   );
 };
